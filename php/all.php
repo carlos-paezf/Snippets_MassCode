df2537515202c091b11efa2f9e55b5b26f8cbf5a
carlos-paezf <carlos.paezf@usantoto.edu.co>
2020-10-31 20:52:11 -0500

    All

---
+++ php/all.php
@@ -0,0 +1,11 @@
+Returns true if the provided function returns true for all elements of an array, false otherwise.
+
+<?php
+function all($items, $func)
+{
+  return count(array_filter($items, $func)) === count($items);
+}
+
+all([2, 3, 4, 5], function ($item) {
+  return $item > 1;
+}); // true
