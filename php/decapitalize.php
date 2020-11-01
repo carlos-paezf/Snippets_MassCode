1efbd274e503f47d0ead3fac82a95451c5727261
carlos-paezf <carlos.paezf@usantoto.edu.co>
2020-10-31 20:58:07 -0500

    Decapitalize

---
+++ php/decapitalize.php
@@ -0,0 +1,14 @@
+Decapitalizes the first letter of a string.
+Decapitalizes the first letter of the string and then adds 
+it with rest of the string. Omit the upperRest parameter 
+to keep the rest of the string intact, or set it to true 
+to convert to uppercase.
+
+<?php
+function decapitalize($string, $upperRest = false)
+{
+  return lcfirst($upperRest ? strtoupper($string) : $string);
+}
+
+decapitalize('FooBar'); // 'fooBar'
+?>
