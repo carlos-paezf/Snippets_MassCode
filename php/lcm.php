32dd485bf66b961d7e8c3c735d196ec2a8a89b36
carlos-paezf <carlos.paezf@usantoto.edu.co>
2020-10-31 21:05:47 -0500

    LCM

---
+++ php/lcm.php
@@ -0,0 +1,16 @@
+Returns the least common multiple of two or more numbers.
+
+<?php
+
+function lcm(...$numbers)
+{
+    $ans = $numbers[0];
+    for ($i = 1, $max = count($numbers); $i < $max; $i++) {
+        $ans = (($numbers[$i] * $ans) / gcd($numbers[$i], $ans));
+    }
+
+    return $ans;
+}
+
+lcm(12, 7); // 84
+lcm(1, 3, 4, 5); // 60
