cbf96e487b953d22d95ee901d753910b0aae5563
carlos-paezf <carlos.paezf@usantoto.edu.co>
2020-10-31 20:53:30 -0500

    Any

---
+++ php/any.php
@@ -0,0 +1,17 @@
+Returns true if the provided function returns true for at 
+least one element of an array, false otherwise.
+
+<?php
+function any(array $items, callable $func){
+  foreach ($items as $item){
+    if ($func($item)){
+      return true;
+    }
+  }
+  return false;
+}
+
+any([1, 2, 3, 4], function ($item) {
+  return $item < 2;
+}); // true
+?>
