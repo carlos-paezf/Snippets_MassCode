4776348866c4eb985a57b4560ccca53fc59bc677
carlos-paezf <carlos.paezf@usantoto.edu.co>
2020-10-31 20:57:19 -0500

    Count Vowels

---
+++ php/countVowels.php
@@ -0,0 +1,14 @@
+Returns number of vowels in provided string.
+Use a regular expression to count the number of vowels 
+(A, E, I, O, U) in a string.
+
+<?php
+function countVowels($string)
+{
+  preg_match_all('/[aeiou]/i', $string, $matches);
+
+  return count($matches[0]);
+}
+
+countVowels('sampleInput'); // 4
+?>
