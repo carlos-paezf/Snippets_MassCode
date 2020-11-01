09facd6161cd882d8e2bd3736068702e43b838a5
carlos-paezf <carlos.paezf@usantoto.edu.co>
2020-10-31 21:09:57 -0500

    Remove

---
+++ php/remove.php
@@ -0,0 +1,16 @@
+Removes elements from an array for which the given function 
+returns false.
+
+<?php
+function remove($items, $func)
+{
+  $filtered = array_filter($items, $func);
+
+  return array_diff_key($items, $filtered);
+}
+
+remove([1, 2, 3, 4], function ($n) {
+  return ($n % 2) === 0;
+});
+// [0 => 1, 2 => 3]
+?>
