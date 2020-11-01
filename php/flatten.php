40d6c04972a0213bf6a4afd4b82c1ebf2c859da5
carlos-paezf <carlos.paezf@usantoto.edu.co>
2020-10-31 21:00:26 -0500

    Flatten

---
+++ php/flatten.php
@@ -0,0 +1,19 @@
+Flattens an array up to the one level depth.
+
+<?php
+function flatten($items)
+{
+  $result = [];
+  foreach ($items as $item) {
+    if (!is_array($item)) {
+      $result[] = $item;
+    } else {
+      $result = array_merge($result, array_values($item));
+    }
+  }
+
+  return $result;
+}
+
+flatten([1, [2], 3, 4]); // [1, 2, 3, 4]
+?>
