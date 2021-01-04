8a00587c876f74bd36930f04bfbb9eacb3f6f785
carlos-paezf <carlos.paezf@usantoto.edu.co>
2021-01-04 14:15:44 -0500

    Split lines

---
+++ javascript/splitLines.js
@@ -0,0 +1,7 @@
+//Splits a multiline string into an array of lines.
+//Use String.prototype.split() and a regular expression to match line breaks and create an array.
+
+
+const splitLines = str => str.split(/\r?\n/);
+
+splitLines('This\nis a\nmultiline\nstring.\n'); // ['This', 'is a', 'multiline', 'string.' , '']
